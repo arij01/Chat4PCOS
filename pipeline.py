0a41acb4b02ad6df693698b37d3ee235acf7bc4f
arij <m'hiri.arij@esprit.tn>
2024-12-26 11:42:04 +0100

    problem with huggingface token

--- pipeline.py
+++ pipeline.py
@@ -6,14 +6,15 @@ uri = "bolt://localhost:7687"
 username = "neo4j"  
 password = "chatbotchatbot"  
 
-TOKEN = os.getenv("HUGGINGFACE_TOKEN")
+# TOKEN = os.getenv("HUGGINGFACE_TOKEN")
+TOKEN="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"
 # Initialize the Hugging Face pipeline
-generator = pipeline('text-generation', model='gpt-2', use_auth_token=TOKEN)
+# generator = pipeline('text-generation', model='gpt-2', use_auth_token=TOKEN)
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
-def query_database(query, parameters):
+def query_database(query):
     with driver.session() as session:
-        result = session.run(query, parameters)
+        result = session.run(query)
         return [record.data() for record in result]
 
 def load_query(file_path):
@@ -22,29 +23,17 @@ def load_query(file_path):
 
 
 query = load_query("data/queries/pcos_query.cypher")
-parameters = {"patient_id": 1}
-data = query_database(query, parameters)
 
+data = query_database(query)
+print(data)
 
-# Ensure data exists
-if not data:
-    print("No data found for the specified patient ID.")
-else:
-    # Extract data from the query results
-    record = data[0]  # Assuming one patient at a time
-    patient = record.get("p", {})
-    conditions = record.get("conditions", [])
-    hormones = record.get("hormones", [])
-    risk_factors = record.get("risk_factors", [])
-
-    # Construct a prompt summarizing graph data
-    prompt = "Hello"
+prompt = "Hello"
 
 # Generate response from Hugging Face
-    response = generator(prompt, max_length=500, num_return_sequences=1)
+response = generator(prompt, max_length=500, num_return_sequences=1)
 
     # Print the response
-    print(response[0]['generated_text'])
+print(response[0]['generated_text'])
 
 # Close the Neo4j driver
 #driver.close()
